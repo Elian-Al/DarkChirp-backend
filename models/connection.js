b0583bacf994783d80a1ebf981e32f3ccdf925e3
Elian <elian.alphonse@gmail.com>
2026-06-14 14:42:52 +0200

    fix(connection): mettre à jour les options de connexion à la base de données

--- models/connection.js
+++ models/connection.js
@@ -2,7 +2,15 @@ const mongoose = require("mongoose");
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
+const mongoOptions = {
+  maxPoolSize: 5,
+  minPoolSize: 1,
+  socketTimeoutMS: 30000,
+  serverSelectionTimeoutMS: 5000,
+  // connectTimeoutMS: 10000
+};
+
 mongoose
-  .connect(MONGODB_URI, { connectTimeoutMS: 10000 })
+  .connect(MONGODB_URI, mongoOptions)
   .then(() => console.log("Database connected"))
   .catch((error) => console.error(error));
